6d640c842074219b1e7dfe699cecaa52ea82ea1a
iTsGiuse <giuse.amato@icloud.com>
2024-05-23 16:16:07 +0200

    feat: created and populated database; add header

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Dc Comics</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-2">
+                    <img src="{{Vite::asset('resources/img/dc-logo.png') }}" alt="">
+                </div>
+                <div class="col-2">
+                    <div class="d-flex">
+                        <ul>
+                            <li>
+                                <a href="http://">Movies</a>
+                            </li>
+                            <li>
+                                <a href="http://">Aggiungi</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>   
+        </div>
+    </header>
+    <main>
+        
+    </main>
+
+</body>
+
+</html>
